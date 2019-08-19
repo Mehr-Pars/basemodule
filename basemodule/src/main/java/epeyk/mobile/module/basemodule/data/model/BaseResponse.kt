028ed15dc78e9b42882b72c9e13424e08a05f8da
bootiyar <chenarani.majid@gmail.com>
2019-08-19 10:19:51 +0430

    develop - edit base

--- basemodule/src/main/java/epeyk/mobile/module/basemodule/data/model/BaseResponse.kt
+++ basemodule/src/main/java/epeyk/mobile/module/basemodule/data/model/BaseResponse.kt
@@ -2,10 +2,10 @@ package epeyk.mobile.module.basemodule.data.model
 
 import com.google.gson.Gson
 
-open class BaseModel : ErrorResponse() {
+open class BaseResponse : ErrorResponse() {
     fun toJsString(): String = Gson().toJson(this).toString()
 
     companion object {
-        open fun toObject(str: String): BaseModel = Gson().fromJson(str, BaseModel::class.java)
+        open fun toObject(str: String): BaseResponse = Gson().fromJson(str, BaseResponse::class.java)
     }
 }
