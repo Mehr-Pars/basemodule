5bcf4a7b28d22f241c515a5b702099f84688967e
Ali <aliarasteh93@gmail.com>
2019-08-25 21:57:52 +0430

    تغییر BaseConfig

--- basemodule/src/main/java/epeyk/mobile/module/basemodule/data/network/BaseConfig.kt
+++ basemodule/src/main/java/epeyk/mobile/module/basemodule/data/network/BaseConfig.kt
@@ -4,15 +4,15 @@ package epeyk.mobile.module.basemodule.data.network
  * Created by bootiyar on 8/18/2019 - 9:42 AM.
  * Project Name: BaseModuleHolder.
  */
-abstract class BaseConfig {
-    abstract var baseUrl: String?
-    abstract var apiUrl: String?
-    abstract var authToken: String?
-    abstract var profileId: String?
-    abstract var clubId: String?
-    abstract var storeId: String?
-    abstract var communityId: String?
-    abstract var state: String?
-    abstract var version: String?
-    abstract var directoryPath: String?
+open class BaseConfig {
+    open var baseUrl: String? = ""
+    open var apiUrl: String? = ""
+    open var authToken: String? = ""
+    open var profileId: String? = ""
+    open var clubId: String? = ""
+    open var storeId: String? = ""
+    open var communityId: String? = ""
+    open var state: String? = ""
+    open var version: String? = ""
+    open var directoryPath: String? = ""
 }
